052bba1151c34356863530a69c67ecbbd5184b3b
DESKTOP-2H5MSTF\Gooseberry <3105394133@qq.com>
2021-06-08 01:12:46 +0800

    an update for character

--- character.cpp
+++ character.cpp
@@ -2,19 +2,61 @@
 
 USING_NS_CC;
 
-void character::move() {
-    ;
+character::~character() {}
+
+Sprite* character::getSprite() { return this->sprite; }
+
+void character::bindSprite(Sprite* sprite, int layer) {
+	this->sprite = sprite;
+	this->sprite->setGlobalZOrder(layer);
+
+	Size size = this->sprite->getContentSize();
+	this->setContentSize(size); 
+
+	this->setAnchorPoint(Point(0.5f, 0.5f)); 
+	setPosition(Point(.0f, .0f));
+
+	this->addChild(sprite);
+	sprite->setPosition(Point(size.width / 2, size.height / 2));
 }
 
-void character::attack() {
-    ;
+void character::showDeathEffect() {
+	auto blink = Blink::create(0.5f, 3);
+	auto FadeOut = FadeOut::create(0.5f);
+	auto callFunc = CallFunc::create([&]() {
+		this->retain();
+		this->removeFromParent();
+		});
+
+	auto sequence = Sequence::create(Spawn::create(blink, FadeOut, NULL), callFunc, NULL);
+	this->getSprite()->runAction(sequence);
 }
 
-bool character::isdead()
-{
-    return _hp <= 0;
+void character::getdamage(int damage) { 
+	if (getSprite() == nullptr) return;
+	if (this->HP > damage)
+		this->HP -= damage;
+	else
+		this->HP = 0;
+
+	/*受伤特效*/
+	/*FlowWord* flowWord = FlowWord::create();
+	this->addChild(flowWord);
+	flowWord->showWord(-delta,
+		getSprite()->getPosition() +
+		Vec2(0, this->getContentSize().height / 2.2f));*/
 }
-void  character::getdamage(int damage)
-{
-    _hp -= damage;
-}
+
+int character::getHP() const { return this->HP; }
+
+void character::setHP(int HP) { this->HP = std::min(HP, maxHP); }
+
+int character::getMaxHP() const { return this->maxHP; }
+
+void character::setMaxHP(int maxHP) { this->maxHP = maxHP; }
+
+float character::getMoveSpeed() const { return moveSpeed; }
+
+void character::setMoveSpeed(float speed) { this->moveSpeed = speed; }
+
+bool character::isdead() const { return HP<=0; }
--- character.h
+++ character.h
@@ -1,31 +1,41 @@
 #ifndef _CHARACTER_
 #define _CHARACTER_
+
 #include "cocos2d.h"
 
 USING_NS_CC;
 
-class character :public Node
-{
+class character :public Node {
 public:
-	virtual void move();
-	virtual void attack();
+	character() = default;
 
-	virtual void setHP(int HP) { _hp = (HP > * _hp_max) ? *_hp_max : HP; }
-	virtual void setHPMax(int HPMax) { *_hp_max = HPMax; }
-	virtual int getHP()const { return _hp; }
-	virtual int getHPMax()const { return *_hp_max; }
-	
-	virtual void setSpeed(float speed) { *_speed = speed; }
-	virtual float getSpeed()const { return *_speed; }
+	Sprite* getSprite();
 
-	virtual void getdamage(int damage)=0;
-	void bindSprite(Sprite* sprite) { _player = sprite; addChild(_player); };
-	bool isdead();
+	void bindSprite(Sprite* sprite, int layer);
+
+	virtual void showDeathEffect();
+
+	void setMaxHP(int maxhp);
+	int getMaxHP() const;
+	void setHP(int hp);
+	int getHP() const;
+	void setMoveSpeed(float movespeed);
+	float getMoveSpeed() const;
+
+	virtual void getdamage(int damage);
+
+	bool isdead() const;
+
+	~character();
+
+private:
+	Sprite* sprite;
 protected:
-	std::shared_ptr<float> _hp_max;
-	std::shared_ptr<float> _speed;
-	float _hp;
-	float _armor;
-	cocos2d::Sprite* _player;
+	int attack = 3;
+	int HP = 10, maxHP = 10;
+	//float attackSpeed = 0.0f;  
+	float moveSpeed = 0.0f;  
+
 };
-#endif
+
+#endif //_CHARACTER_
